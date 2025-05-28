fc3b8dae52095b75d8d7a4e6a5a00a8a1239c36f
Default <hedgehog2408@gmail.com>
2025-05-28 08:53:58 +0300

    They are bad painting

---
+++ src/main/java/org/sta6/dev/third_project_kp/ThirdProjectKpApplication.java
@@ -0,0 +1,13 @@
+package org.sta6.dev.third_project_kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdProjectKpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdProjectKpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sta6/dev/third_project_kp/ThirdProjectKpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sta6.dev.third_project_kp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdProjectKpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
